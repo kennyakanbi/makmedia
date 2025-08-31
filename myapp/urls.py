bc0bd2519e981871920cc00afc658c44977be694
Kenny Akanbi <kennyakanbi@gmail.com>
2025-08-31 11:20:30 +0100

    more fixing of urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -10,3 +10,13 @@ urlpatterns = [
     path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
     path('intern/', views.internshipdetails, name='intern'),
 ]
+
+from django.views.generic import RedirectView
+
+urlpatterns += [
+    path('about', RedirectView.as_view(url='/about/', permanent=True)),
+    path('contact', RedirectView.as_view(url='/contact/', permanent=True)),
+    path('services', RedirectView.as_view(url='/services/', permanent=True)),
+    path('intern', RedirectView.as_view(url='/intern/', permanent=True)),
+    path('blog', RedirectView.as_view(url='/blog/', permanent=True)),
+]
