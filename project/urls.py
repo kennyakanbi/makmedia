85a17c2e110b2f5c9cfe20df27d91cfdd3d4dfa7
Kenny Akanbi <kennyakanbi@gmail.com>
2025-09-08 14:23:41 +0100

    updates on number of pages

--- project/urls.py
+++ project/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('admin', RedirectView.as_view(url='/admin/', permanent=True)),  # redirect /admin → /admin/
     path('', include('myapp.urls')),
     path('auth/', include('main.urls')),
   
