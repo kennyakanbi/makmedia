c53515cb33c91591f52e9b08f2c0e8128888fd58
Kenny Akanbi <kennyakanbi@gmail.com>
2025-12-04 11:40:53 +0100

    new commit

--- project/urls.py
+++ project/urls.py
@@ -27,6 +27,5 @@ urlpatterns = [
     path('auth/', include('main.urls')),
   
 ] 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in all environments (ok for small projects / testing)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
