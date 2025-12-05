01765f6c4d0eecd8854b6849ebfae5895e2505bf
Kenny Akanbi <kennyakanbi@gmail.com>
2025-12-05 10:26:06 +0100

    new update

--- project/urls.py
+++ project/urls.py
@@ -1,19 +1,4 @@
-"""
-URL configuration for project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# project/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -22,10 +7,12 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin', RedirectView.as_view(url='/admin/', permanent=True)),  # redirect /admin → /admin/
+    path('admin', RedirectView.as_view(url='/admin/', permanent=True)),
     path('', include('myapp.urls')),
     path('auth/', include('main.urls')),
-  
-] 
-if settings.DEBUG or True:  # Temporary for production testing
+]
+
+# Serve media files only in DEBUG/development.
+# Remove this block in production if you're serving media via a webserver or object storage.
+if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
